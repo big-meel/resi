3209d0f215b8f75ee13557d18fd2c08fdab62506
big-meel <big.meel@zoho.com>
2021-11-28 23:42:10 -0500

    Update listing scraper

--- lib/resi/scraper/listing_scraper.ex
+++ lib/resi/scraper/listing_scraper.ex
@@ -1,9 +1,11 @@
 defmodule Resi.Scraper.ListingScraper do
   @listings_url "https://raj.xposureapp.com/listing/"
 
-  def fetch_listing_html(url) do
-    HTTPoison.get(url)
+  def fetch_listing_html(id) do
+    HTTPoison.get( format_url(id) )
   end
+
+  # Add methods to get listing information from html
 
   defp format_url(id) do
     "#{@listings_url}#{id}"
