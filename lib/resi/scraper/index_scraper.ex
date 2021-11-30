2499c002489e9d28092f8e203ec4894c0bc84761
big-meel <big.meel@zoho.com>
2021-11-30 02:13:29 -0500

    Add functions to parse url for relevant data

--- lib/resi/scraper/index_scraper.ex
+++ lib/resi/scraper/index_scraper.ex
@@ -1,12 +1,39 @@
 defmodule Resi.Scraper.IndexScraper do
   @raj_url "https://raj.xposureapp.com/"
 
-  def fetch_html do
-    HTTPoison.get(@raj_url)
+
+  def get_list_of_ids() do
+    fetch_html()
+    |> parse_response()
+    |> get_id_script()
+    |> get_raw_id_binary()
   end
 
-  def get_ids do
-    # Scrape html for ids
-    # Return list of IDS
+  defp fetch_html() do
+   case HTTPoison.get(@raj_url) do
+    {:ok, response} ->
+      response.body
+    {:error, reason} ->
+      reason
+   end
+  end
+
+  defp parse_response(html) do
+    case Floki.parse_document(html) do
+      {:ok, list_of_nodes} ->
+        list_of_nodes
+      {:error, reason} ->
+        reason
+    end
+  end
+
+  defp get_id_script(node_list) do
+    Floki.find(node_list, "script")
+  end
+
+  defp get_raw_id_binary(node) do
+    node
+    |> List.last
+    |> Floki.children
   end
 end
