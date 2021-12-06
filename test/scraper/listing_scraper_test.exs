3387e978f069a722cc0c60c2ce23836309d380fb
big-meel <big.meel@zoho.com>
2021-12-05 23:59:44 -0500

    Add Tests for listing scraper

---
+++ test/scraper/listing_scraper_test.exs
@@ -0,0 +1,26 @@
+defmodule Resi.Scraper.ListingScraperTest do
+  use ExUnit.Case, async: true
+
+  alias Resi.Scraper.ListingScraper, as: Scraper
+
+  @id "1upgs"
+
+  setup do
+    details = Scraper.fetch_listing_html(@id)
+    |> Scraper.list_nodes()
+    |> Scraper.fetch_details()
+
+    {:ok, details: details}
+  end
+
+  describe "Scraper.fetch_details/1" do
+
+
+    test "with valid id gets a map containing details", context do
+      assert is_map(context[:details])
+      assert Map.has_key? context[:details], "Address"
+    end
+
+  end
+
+end
